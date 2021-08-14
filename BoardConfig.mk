345382047cdf425e757d9d0c21ff19815e69395b
Sekharbobby <sekhar0199@gmail.com>
2021-08-14 16:41:20 +0530

    apollo: rename libinit to apollo

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,8 +28,8 @@ BUILD_BROKEN_DUP_RULES := true
 TARGET_SCREEN_DENSITY := 440
 
 # Init
-TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollon
-TARGET_RECOVERY_DEVICE_MODULES := libinit_apollon
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_apollo
+TARGET_RECOVERY_DEVICE_MODULES := libinit_apollo
 
 # Kernel
 TARGET_KERNEL_CONFIG := vendor/apollo_defconfig
