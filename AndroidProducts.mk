477564b5681bc1d527836676c6a74e2598a8a464
Sekharbobby <sekhar0199@gmail.com>
2021-10-31 14:47:06 +0100

    apollo: Cafex Bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/nad_apollo.mk
+    $(LOCAL_DIR)/cafex_apollo.mk
 
 COMMON_LUNCH_CHOICES := \
-    nad_apollo-user \
-    nad_apollo-userdebug \
-    nad_apollo-eng
+    cafex_apollo-user \
+    cafex_apollo-userdebug \
+    cafex_apollo-eng
--- cafex_apollo.mk
+++ cafex_apollo.mk
@@ -11,18 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from apollo device
 $(call inherit-product, device/xiaomi/apollo/device.mk)
 
-# Inherit some common NusantaraProject stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common CAFEX stuff.
+$(call inherit-product, vendor/extended/common.mk)
 
-TARGET_USES_BLUR := true
-USE_PIXEL_CHARGING := true
-NAD_BUILD_TYPE ?= UNOFFICIAL
+#TARGET_USES_BLUR := true
+#USE_PIXEL_CHARGING := true
 TARGET_BOOT_ANIMATION_RES := 1080
 USE_GAPPS ?= true
-USE_AOSP_CLOCK := true
+#USE_AOSP_CLOCK := true
 
 # Product
-PRODUCT_NAME := nad_apollo
+PRODUCT_NAME := cafex_apollo
 PRODUCT_DEVICE := apollo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
